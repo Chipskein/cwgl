87a23cf581d54e707a46990a9eb25c44205b7919
Chipskein <abfn0905@gmail.com>
2024-06-22 15:34:36 -0300

    feat:login working

--- src/main.zig
+++ src/main.zig
@@ -3,23 +3,16 @@ const ray = @import("raylib.zig");
 
 const WIDTH = 750;
 const HEIGHT = 750;
-const FPS = 60;
+const FPS = 12;
 const PIXEL_SIZE = 25;
 const TITLE = "Conway game of life";
 const BACKGROUND_COLOR = ray.BLACK;
-const ROWS = HEIGHT / PIXEL_SIZE;
-const COLUMNS = WIDTH / PIXEL_SIZE;
+const ROWS: comptime_int = HEIGHT / PIXEL_SIZE;
+const COLUMNS: comptime_int = WIDTH / PIXEL_SIZE;
 var CELLS: [ROWS][COLUMNS]i32 = undefined;
+var CELLS_TEMP: [ROWS][COLUMNS]i32 = undefined;
 
-fn setState(state: bool, row: usize, column: usize) !void {
-    if (state) {
-        CELLS[row][column] = 1;
-    } else {
-        CELLS[row][column] = 0;
-    }
-}
-
-fn drawGrid() !void {
+fn draw_grid() !void {
     const colorRecOff = ray.GRAY;
     const colorRecOn = ray.GREEN;
     for (0..@intCast(ROWS)) |y| {
@@ -38,26 +31,78 @@ fn drawGrid() !void {
         }
     }
 }
-fn updateState() !void {
-    //Apply Conway's rules
+fn get_alive_neighbours(row: usize, column: usize) i32 {
+    var live: i32 = 0;
+    const neighbours_offset = [8][2]i32{
+        [_]i32{ -1, 0 }, // Directly above
+        [_]i32{ 1, 0 }, // Directly below
+        [_]i32{ 0, -1 }, // To the left
+        [_]i32{ 0, 1 }, // To the right
+        [_]i32{ -1, -1 }, // Diagonal upper left
+        [_]i32{ -1, 1 }, // Diagonal upper right
+        [_]i32{ 1, -1 }, // Diagonal lower left
+        [_]i32{ 1, 1 }, // Diagonal lower right
+    };
+    for (neighbours_offset) |offset| {
+        const y: i32 = @intCast(row);
+        const x: i32 = @intCast(column);
+        const y_offset: i32 = @intCast(offset[0]);
+        const x_offset: i32 = @intCast(offset[1]);
+        const neighbor_row: usize = @intCast(@mod((y + y_offset + ROWS), ROWS));
+        const neighbor_column: usize = @intCast(@mod((x + x_offset + COLUMNS), COLUMNS));
+        const neighbor = CELLS[neighbor_row][neighbor_column];
+        if (neighbor == 1) {
+            live += 1;
+        }
+    }
+    return live;
+}
+fn update_state() !void {
+    CELLS_TEMP = CELLS;
+    //RULES:
     //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    //Any live cell with two or three live neighbours lives on to the next generation.
     //Any live cell with more than three live neighbours dies, as if by overpopulation.
+    //Any live cell with two or three live neighbours lives on to the next generation.
     //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
+    for (CELLS_TEMP, 0..) |row, row_index| {
+        for (0..row.len) |column_index| {
+            const alive_neighbours = get_alive_neighbours(row_index, column_index);
+            const cell = CELLS_TEMP[row_index][column_index];
+            const isAlive = cell == 1;
+            if (isAlive) {
+                if (alive_neighbours < 2 or alive_neighbours > 3) {
+                    CELLS_TEMP[row_index][column_index] = 0;
+                }
+                if (alive_neighbours == 2 or alive_neighbours == 3) {
+                    CELLS_TEMP[row_index][column_index] = 1;
+                }
+            } else {
+                if (alive_neighbours == 3) {
+                    CELLS_TEMP[row_index][column_index] = 1;
+                }
+            }
+        }
+    }
+    CELLS = CELLS_TEMP;
 }
 pub fn main() !void {
     ray.SetConfigFlags(ray.FLAG_MSAA_4X_HINT | ray.FLAG_VSYNC_HINT);
     ray.InitWindow(WIDTH, HEIGHT, TITLE);
     defer ray.CloseWindow();
     ray.SetTargetFPS(FPS);
-    //try setState(true, ROWS / 2, COLUMNS / 2);
+    //create a init state function
+    CELLS[2][1] = 1;
+    CELLS[3][2] = 1;
+    CELLS[4][0] = 1;
+    CELLS[4][1] = 1;
+    CELLS[4][2] = 1;
     while (!ray.WindowShouldClose()) {
         if (ray.IsKeyPressed(ray.KEY_ESCAPE)) ray.CloseWindow();
         ray.BeginDrawing();
         defer ray.EndDrawing();
         ray.ClearBackground(BACKGROUND_COLOR);
-        try drawGrid();
+        try draw_grid();
+        try update_state();
     }
 }
 
