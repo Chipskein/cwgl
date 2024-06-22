72cf32cdcac322ae51a4dd3fbb2ac4c2dfbe08ce
Chipskein <abfn0905@gmail.com>
2024-06-21 22:19:26 -0300

    feat:grid

--- src/main.zig
+++ src/main.zig
@@ -1,60 +1,46 @@
 const std = @import("std");
 const ray = @import("raylib.zig");
 
-pub fn main() !void {
-    try ray_main();
-    try hints();
+fn drawGrid(rows: i32, colums: i32, cellSize: i32) !void {
+    const colorRecOff = ray.GRAY;
+    //const colorRecOn = ray.GREEN;
+    for (0..@intCast(rows)) |y| {
+        for (0..@intCast(colums)) |x| {
+            const row: i32 = @intCast(y);
+            const column: i32 = @intCast(x);
+            const color = colorRecOff;
+            const posX = row * cellSize;
+            const posY = column * cellSize;
+            const recSize = cellSize - 1;
+            ray.DrawRectangle(posX, posY, recSize, recSize, color);
+        }
+    }
 }
-
-fn ray_main() !void {
+pub fn main() !void {
     const width = 750;
     const height = 750;
-    const FPS=60;
-    //const pixelSize=25;
-    const title="Conway game of life";
-    var bufferData:[height][width]bool =undefined;
-    for (0.., bufferData) |y, row| {
-        for (0..,row) |x, _| {
-            bufferData[y][x]=false;
-        }   
-    }
+    const FPS = 60;
+    const pixelSize = 25;
+    const title = "Conway game of life";
+    const backgroundColor = ray.BLACK;
+    const rows = height / pixelSize;
+    const colums = width / pixelSize;
     ray.SetConfigFlags(ray.FLAG_MSAA_4X_HINT | ray.FLAG_VSYNC_HINT);
     ray.InitWindow(width, height, title);
     defer ray.CloseWindow();
     ray.SetTargetFPS(FPS);
-    //var prng = std.rand.DefaultPrng.init(0);
-    //const rand = prng.random();
-    bufferData[0][0]=true;    
     while (!ray.WindowShouldClose()) {
         if (ray.IsKeyPressed(ray.KEY_ESCAPE)) ray.CloseWindow();
         ray.BeginDrawing();
         defer ray.EndDrawing();
-        ray.ClearBackground(ray.BLACK);
-        //draw buffer
-        for (0.., bufferData) |y, row| {
-            for (0..,row) |x, cell| {
-                std.debug.print("{} {} {}\n",.{x,y,cell});
-                if (x%2==0){
-                    ray.DrawRectangle(@intCast(x),@intCast(y), 25,25, ray.GREEN);
-                }  else {
-                    ray.DrawRectangle(@intCast(x),@intCast(y), 25,25, ray.BLACK);
-                }
-            }   
-        }
+        ray.ClearBackground(backgroundColor);
+        try drawGrid(rows, colums, pixelSize);
     }
 }
 
-fn hints() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n", .{});
-    try stdout.print("Here are some hints:\n", .{});
-    try stdout.print("Run `zig build --help` to see all the options\n", .{});
-    try stdout.print("Run `zig build -Doptimize=ReleaseSmall` for a small release build\n", .{});
-    try stdout.print("Run `zig build -Doptimize=ReleaseSmall -Dstrip=true` for a smaller release build, that strips symbols\n", .{});
-    try stdout.print("Run `zig build -Draylib-optimize=ReleaseFast` for a debug build of your application, that uses a fast release of raylib (if you are only debugging your code)\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
+//try stdout.print("\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n", .{});
+//try stdout.print("Here are some hints:\n", .{});
+//try stdout.print("Run `zig build --help` to see all the options\n", .{});
+//try stdout.print("Run `zig build -Doptimize=ReleaseSmall` for a small release build\n", .{});
+//try stdout.print("Run `zig build -Doptimize=ReleaseSmall -Dstrip=true` for a smaller release build, that strips symbols\n", .{});
+//try stdout.print("Run `zig build -Draylib-optimize=ReleaseFast` for a debug build of your application, that uses a fast release of raylib (if you are only debugging your code)\n", .{});
